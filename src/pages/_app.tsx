c2c570f5e27507fb79de21047b87aa5d564c4d3d
Lukasz Wisniewski <lukasz.wisniewski48@wp.pl>
2023-01-29 02:54:52 +0100

    feat: apollo-client provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import type { AppProps } from "next/app";
+
+const client = new ApolloClient({
+  uri: "https://countries.trevorblades.com/",
+  cache: new InMemoryCache(),
+  resolvers: {},
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
