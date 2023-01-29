3a3a554c54550caab0bd6ac68e87aafb6a4deed2
Lukasz Wisniewski <lukasz.wisniewski48@wp.pl>
2023-01-29 21:14:35 +0100

    Fix: Change fetch from apollo to gQL request

--- src/pages/countries/[code].tsx
+++ src/pages/countries/[code].tsx
@@ -1,16 +1,37 @@
 import Layout from "@/Components/Layout";
+import { useState, useEffect } from "react";
 import { NextPage } from "next";
-import * as React from "react";
-import { useGetSingleCountryQuery } from "types-and-hooks";
+import { GetSingleCountryQuery } from "types-and-hooks";
+import { graphRequestFetcher } from "../graphRequestFetcher";
 
 interface ICountryProps {}
 
 const Country: NextPage<{ code: string }> = ({ code }) => {
-  const { data, loading, error } = useGetSingleCountryQuery({
-    variables: { ID: code },
-  });
+  const [data, setData] = useState<GetSingleCountryQuery>({});
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string>("");
+
+  // const { data, loading, error } = useGetSingleCountryQuery({
+  //   variables: { ID: code },
+  // });
+
+  const fetchCountry = async () => {
+    try {
+      const sdk = await graphRequestFetcher();
+      const { data } = await sdk.getSingleCountry({ ID: code });
+      setData(data);
+    } catch (error: any) {
+      console.error(JSON.stringify(error, undefined, 2));
+      setError(JSON.stringify(error.message));
+    }
+    setLoading(false);
+  };
+  useEffect(() => {
+    fetchCountry();
+  }, []);
+
   if (loading) return <Layout>Loading...</Layout>;
-  if (error) return <Layout>Error! ${error.message}</Layout>;
+  if (error) return <Layout>Error! ${error}</Layout>;
 
   return (
     <Layout>
@@ -26,10 +47,10 @@ const Country: NextPage<{ code: string }> = ({ code }) => {
           borderRadius: "10px",
         }}
       >
-        <span>{data?.country?.code}</span>
-        <span>{data?.country?.name}</span>
-        <span>{data?.country?.emoji}</span>
-        <span>{data?.country?.languages[0]?.name}</span>
+        <span>{data.country?.code}</span>
+        <span>{data.country?.name}</span>
+        <span>{data.country?.emoji}</span>
+        <span>{data.country?.languages[0]?.name}</span>
       </div>
     </Layout>
   );
